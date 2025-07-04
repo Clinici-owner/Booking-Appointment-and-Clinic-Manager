e833c5a15968452ff760b8bda3625db992d57e90
Minh Hiển <minhhien30201@gmail.com>
2025-07-05 02:45:51 +0700

    update

--- Backend/src/app/controllers/doctorController.js
+++ Backend/src/app/controllers/doctorController.js
@@ -69,5 +69,21 @@ class doctorController {
       res.status(500).json({ message: "Internal server error" });
     }
   }
+
+  // get doctor by specialty
+  async getDoctorsBySpecialty(req, res) {
+    const { specialtyId } = req.params;
+    try {
+      const doctors = await DoctorProfile.find({ specialties: specialtyId })
+        .populate("doctorId specialties");
+      if (!doctors || doctors.length === 0) {
+        return res.status(404).json({ message: "No doctors found for this specialty" });
+      }
+      res.status(200).json(doctors);
+    } catch (error) {
+      console.error("Error fetching doctors by specialty:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
 }
 module.exports = new doctorController();
