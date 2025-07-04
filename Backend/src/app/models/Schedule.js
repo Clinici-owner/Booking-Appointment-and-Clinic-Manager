b19ad651edc93e77db2922aa6ff95e98869a3fdc
Erikrussian <caohoanglinh203@gmail.com>
2025-07-05 00:56:33 +0700

    update

--- Backend/src/app/models/Schedule.js
+++ Backend/src/app/models/Schedule.js
@@ -3,8 +3,7 @@ const Schema = mongoose.Schema;
 
 const Schedule = new Schema({
     userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    room: { type: Schema.Types.ObjectId, ref: 'Room', required: true },
-    specialties: { type: Schema.Types.ObjectId, ref: 'Specialty', required: false },
+    room: { type: Schema.Types.ObjectId, ref: 'Room', required: true }, //hiển kiêu xóa chuyên khoa
     shift: { type: String, enum: ['MORNING', 'AFTERNOON', 'EVENING'], required: true },
     date: { type: Date, required: true },
 }, { timestamps: true });
