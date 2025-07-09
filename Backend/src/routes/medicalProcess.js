d220e1d69c995df290e69bd488e9b2b07a5e9d6c
TranHoangAnh <thathatha2003@gmail.com>
2025-07-09 17:03:18 +0700

    update process controller

--- Backend/src/routes/medicalProcess.js
+++ Backend/src/routes/medicalProcess.js
@@ -7,10 +7,8 @@ router.post('/create', medicalProcessController.createMedicalProcess);
 router.post('/update/:processId', medicalProcessController.updateMedicalProcess);
 router.post('/updateStatus/:processId', medicalProcessController.updateMedicalProcessStatus);
 router.get('/:processId', medicalProcessController.getMedicalProcessById);
-router.post('/updateCurrentStep/:processId', medicalProcessController.updateMedicalProcessCurrentStep);
 
 router.post('/step/create', medicalProcessController.createProcessStep);
-router.post('/step/update/:stepId', medicalProcessController.updateProcessStep);
 
 module.exports = router;
 
